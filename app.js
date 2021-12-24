901751750235aa3bd26df3b1d8c7bbf2732622fc
baldeepparihar <75191811+baldeepparihar@users.noreply.github.com>
2021-12-24 11:56:45 -0800

    main design complete. slider functional

---
+++ app.js
@@ -0,0 +1,46 @@
+let init = () => {
+
+    let container = document.getElementsByClassName('slider__container')[0];
+    let slides = document.getElementsByClassName('slider__slide');
+    let circles = document.getElementsByClassName('slider__circle');
+
+    current = 1;
+    time = 6000;
+    console.log(`current: ${current}`);
+
+    // slide animation
+    slides[0].classList.add('slider__slide--active');
+    circles[current - 1].classList.add('slider__circle--filled');
+
+    // elipsis scroll
+    let scrollElipsis = (current) => {
+        for (let i = 0; i < slides.length; i++) {
+            circles[i].classList.remove('slider__circle--filled');
+        }
+        circles[current - 1].classList.add('slider__circle--filled');
+    }
+    let startSliding = () => {
+
+        setInterval(() => {
+            
+            // remove active from position 1 and add it to position 0 in slides HTML collection
+            slides[1].classList.add('slider__slide--active');
+            slides[0].classList.remove('slider__slide--active');
+            // clone first slide
+            container.appendChild(slides[0].cloneNode([true]));
+            // remove first slide after cloning it
+            container.removeChild(slides[0]);
+
+            console.log(`slides: ${slides.length}`);
+            if(current < slides.length) {
+                current++;
+                scrollElipsis(current);
+            } else {
+                current = 1;
+                scrollElipsis(current);
+            }
+        }, time);
+    }
+    startSliding();
+}
+init();
